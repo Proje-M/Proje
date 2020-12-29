58517b4463c73655d5074c2bf5f7130507cd9ea7
Busra Dogan <busradogan099@gmail.com>
2020-12-29 21:55:39 +0300

    changed

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,7 +1,7 @@
 const background = '#171D32';
 const statusBar = '#FFF';
 const primary = '#171D32';
-const topNavText = '#000';
+const topNavText = '#FFF';
 const inActiveTab = 'rgb(143, 155, 179)';
 const ActiveTab = '#000';
 export default {
